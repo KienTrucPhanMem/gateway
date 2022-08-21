fd32ba5f6ff7267357c9b5b658ddd851657b0fe2
unknown <minhan112001@gmail.com>
2022-08-21 16:49:51 +0700

    add rabbitMQ implement to gateway

--- src/api/driver/driver.controller.ts
+++ src/api/driver/driver.controller.ts
@@ -1,27 +1,37 @@
 import axios from "axios";
 import { Request, Response } from "express";
 import { ErrorResponse } from "../../helpers/response";
+import { v4 as uuidv4 } from 'uuid';
+const amqplib = require('amqplib');
+const amqp_url = process.env.CLOUDAMQP_URL || 'amqps://lbyjdacl:whjFKganLPxFsFeyzYA_ipgVkergjEmH@armadillo.rmq.cloudamqp.com/lbyjdacl';
+
 
 const authController = {
   async booking(req: Request, res: Response) {
     const body = req.body;
 
     try {
-      // const registerRes = await axios.post(
-      //   "https://ktpm-authen.herokuapp.com/api/register",
-      //   { phone: body.phone, password: body.password }
-      // );
-      // if (body.password) delete body.password;
-      // await axios.post(
-      //   body.role === "USER"
-      //     ? "https://ktpm-user.herokuapp.com/api/passengers"
-      //     : "https://ktpm-driver.herokuapp.com/api/drivers",
-      //   body
-      // );
-      // return res.send({
-      //   accessToken: registerRes.data.accessToken,
-      //   refreshToken: registerRes.data.refreshToken,
-      // });
+      body.requestID = uuidv4();
+
+      const conn = await amqplib.connect(amqp_url, "heartbeat=60");
+      const ch = await conn.createChannel()
+      const exch = 'EXCHANGE_Gateway';
+      const q = 'QUEUE_Gateway';
+      const rkey = 'Route_Gateway';
+
+      await ch.assertExchange(exch, 'direct', { durable: true }).catch(console.error);
+      await ch.assertQueue(q, { durable: true });
+      await ch.bindQueue(q, exch, rkey);
+      await ch.publish(exch, rkey, Buffer.from(JSON.stringify(body)));
+
+      setTimeout(function () {
+        ch.close();
+        conn.close();
+      }, 500);
+
+      return res.send(
+        "send rabbit ok"
+      );
     } catch (err: any) {
       return ErrorResponse(
         res,
